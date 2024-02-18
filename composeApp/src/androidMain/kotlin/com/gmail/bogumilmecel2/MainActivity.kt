4345095d6717adc0c3f5a6bfa9146a24fb4d2c8f
Bogumił Męcel <bogumilmecel2@gmail.com>
2024-02-18 16:05:37 +0100

    Initial commit

---
+++ composeApp/src/androidMain/kotlin/com/gmail/bogumilmecel2/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.gmail.bogumilmecel2
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContent {
+
+        }
+    }
+}
